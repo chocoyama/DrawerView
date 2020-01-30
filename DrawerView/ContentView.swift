f1c9a8ee5d86de912c19093f5fd2e32f12d5591b
Takuya Yokoyama <chocoyama@yahoo-corp.jp>
2020-01-30 14:24:47 +0900

    first commit

--- DrawerView/ContentView.swift
+++ DrawerView/ContentView.swift
@@ -9,10 +9,98 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectingEmoji: String = ""
+    @State private var isPresentedDrawerView: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack(spacing: 16) {
+                ForEach(["😄", "😩", "🥺"], id: \.self) { emoji in
+                    Button(action: {
+                        self.selectingEmoji = emoji
+                        self.isPresentedDrawerView = true
+                    }) {
+                        Text(emoji)
+                            .font(Font.title)
+                    }
+                }
+            }
+            
+            DrawerView(isPresented: $isPresentedDrawerView) {
+                EmojiView(self.selectingEmoji)
+                    .frame(width: 200)
+            }
+        }
     }
 }
+
+struct EmojiView: View {
+    private let emoji: String
+    
+    init(_ emoji: String) {
+        self.emoji = emoji
+    }
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text(emoji)
+            Spacer()
+        }
+    }
+}
+
+struct MenuView: View {
+    var body: some View {
+        List {
+            ForEach(0..<100) { number in
+                Text("\(number)")
+            }
+        }
+    }
+}
+
+struct DrawerView<Content>: View where Content: View {
+    private struct MaskView: View {
+        var body: some View {
+            Rectangle()
+                .background(Color.black)
+                .opacity(0.2)
+        }
+    }
+    
+    @Binding private var isPresented: Bool
+    private let content: () -> Content
+    
+    init(isPresented: Binding<Bool>, @ViewBuilder content: @escaping () -> Content) {
+        self._isPresented = isPresented
+        self.content = content
+    }
+    
+    var body: some View {
+        GeometryReader { (geometry: GeometryProxy) in
+            ZStack(alignment: .leading) {
+                if self.isPresented {
+                    MaskView()
+                        .transition(.opacity)
+                        .onTapGesture {
+                            self.isPresented = false
+                        }
+                    
+                    self.content()
+                        .padding(geometry.safeAreaInsets)
+                        .background(Color.primary.colorInvert())
+                        .transition(.move(edge: .leading))
+                } else {
+                    self.content()
+                        .hidden()
+                }
+            }
+            .animation(.spring())
+            .edgesIgnoringSafeArea([.top, .bottom])        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
